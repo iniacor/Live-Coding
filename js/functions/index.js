9f5cc9b27010be9b6e05d07c411ef9f64b4ce556
ihor <evroigor86@gmail.com>
2022-06-30 17:59:48 +0300

    add refactoring

--- js/functions/index.js
+++ js/functions/index.js
@@ -1,62 +1,71 @@
-function getSenseOfLife() {
-  return 42;
-}
+// function getSenseOfLife() {
+//   return 42;
+// }
 
-//--- test data
-const res = getSenseOfLife();
-console.log(res);
-//option 2
-console.log(getSenseOfLife());
+// //--- test data
+// const res = getSenseOfLife();
+// console.log(res);
+// //option 2
+// console.log(getSenseOfLife());
 
-const getSquared = num => num * num;
-//----test data
-console.log(getSquared(2));
+// const getSquared = num => num * num;
+// //----test data
+// console.log(getSquared(2));
 
-function sum(firstNum, secondNum) {
-  return firstNum + secondNum;
-}
-//----test data
-console.log(sum(9, 9));
-console.log(sum('f', 9));
+// function sum(firstNum, secondNum) {
+//   return firstNum + secondNum;
+// }
+// //----test data
+// console.log(sum(9, 9));
+// console.log(sum('f', 9));
 
-function printMessage(age) {
-  return `I'm ${age} years old`;
-}
-//----test data
-console.log(printMessage(25));
-console.log(printMessage(true));
-console.log(printMessage('name'));
-console.log(printMessage(-7));
-console.log(printMessage({}));
+// function printMessage(age) {
+//   return `I'm ${age} years old`;
+// }
+// //----test data
+// console.log(printMessage(25));
+// console.log(printMessage(true));
+// console.log(printMessage('name'));
+// console.log(printMessage(-7));
+// console.log(printMessage({}));
 
-const mult = (firstNum, secondNum) => firstNum * secondNum;
-//----test data
-console.log(mult(2, 4));
-console.log(mult(-2, 4));
-console.log(mult(Infinity, 4));
-console.log(mult(-4, 4));
-console.log(mult(4, -4));
+// const mult = (firstNum, secondNum) => firstNum * secondNum;
+// //----test data
+// console.log(mult(2, 4));
+// console.log(mult(-2, 4));
+// console.log(mult(Infinity, 4));
+// console.log(mult(-4, 4));
+// console.log(mult(4, -4));
 
-function sumFunc(from, to) {
-  let result = 0;
-  for (let index = from; index <= to; index += 1) {
-    res += index;
-  }
-  return result;
-}
+// function sumFunc(from, to) {
+//   let result = 0;
+//   for (let index = from; index <= to; index += 1) {
+//     res += index;
+//   }
+//   return result;
+// }
 
-function compareSums(firstFrom, firstTo, secondFrom, secondTo) {
-  return sumFunc(firstFrom, firstTo) > sumFunc(secondFrom, secondTo);
-}
+// function compareSums(firstFrom, firstTo, secondFrom, secondTo) {
+//   return sumFunc(firstFrom, firstTo) > sumFunc(secondFrom, secondTo);
+// }
 
-//----test data
-console.log(compareSums(5, 7));
-console.log(compareSums(7, 5));
+// //----test data
+// console.log(compareSums(5, 7));
+// console.log(compareSums(7, 5));
 
-const getMagicNumber = () => 17;
-//----test data
-console.log(getMagicNumber());
-console.log(getMagicNumber('17'));
-console.log(getMagicNumber(17));
+// const getMagicNumber = () => 17;
+// //----test data
+// console.log(getMagicNumber());
+// console.log(getMagicNumber('17'));
+// console.log(getMagicNumber(17));
 
-console.log(getSquared(sum(10, 20)));
+// console.log(getSquared(sum(10, 20)));
+
+// function getPrimes(num) {
+//   getnumber: for (let i = 2; i <= num; i++) {
+//     for (let j = 2; j < i; j++) {
+//       if (i % j === 0) continue getnumber;
+//     }
+//     console.log(i);
+//   }
+// }
---
+++ js/functions/lc6/index.js
@@ -0,0 +1,54 @@
+// 1. Learn requirement (WHAT?)
+// 2. Create step by step algo (& input/output for functions) (HOW?)
+// 3. Write draft solution & testing
+// 4. Refactoring & final testing -> final solution
+//input: number
+//output: undefined
+//algo
+//1. iterate from 2 to num
+//2. check if  N prime
+//2.1 iterate from 2 to N
+//2.2 if N % index === 0 then not prime
+//3. if prime - print it
+
+// function getPrimes(num) {
+//   for (let index = 0; number <= num; index += 1) {
+//     let isPrime = true;
+
+//     for (let index = 2; index < number; index += 1) {
+//       if (number % index === 0) {
+//         isPrime = false;
+//       }
+//     }
+//     if (isPrime) {
+//       console.log(number);
+//     }
+//   }
+// }
+
+//---refactoring---
+
+function isPrime(number) {
+  let isPrime = true;
+  for (let index = 2; index < number; index += 1) {
+    number % index === 0 ? (isPrime = false) : (isPrime = true);
+    if (number % index === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    console.log(number);
+  }
+}
+
+function getPrimes(num) {
+  for (let number = 2; number <= num; number += 1) {
+    isPrime(number);
+  }
+}
+
+//--- testData
+
+getPrimes(15);
+getPrimes(7);
+getPrimes(-7);
